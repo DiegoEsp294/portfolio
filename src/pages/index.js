dae261d07ef124f452f7b0d11b47adc3be044fcf
DiegoEsp294 <diego14esp@hotmail.com>
2021-11-02 19:33:09 -0300

    fixed background

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,8 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 // Components
-/* import Header from "../components/Header"
- */import Work from "../components/Work"
+/* import Header from "../components/Header"*/
+import Work from "../components/Work"
 import About from "../components/about"
 import Skills from "../components/skills"
 import Footer from "../components/Footer"
